80fc1dfcfc4c38c74907feab53ca3399a2405565
parthasarathydNU <parthasarathy.d@northeastern.edu>
2023-06-02 22:09:47 -0400

    Refactored code for fetching collections data

--- index.js
+++ index.js
@@ -17,16 +17,9 @@ app.listen(port, () => {
 })
 
 // end point to get all cities
-app.get('/cities', async (req, res) => {
-    const cities = await getCities();
-    res.send(cities);
-});
+app.get('/cities', async (req, res) => getCities(req, res));
 
 // end point to get data from any given collection
-app.get('/collection/:collectionName', async (req, res) => {
-    const collectionName = req.params.collectionName;
-    const data = await getDataOfCollection(collectionName);
-    res.send(data);
-});
+app.get('/collection/:collectionName', async (req, res) => getDataOfCollection(req, res));
 
 
--- routeHandlers.js
+++ routeHandlers.js
@@ -4,15 +4,25 @@ const { getCitiesFromFirebase, getDataOfCollectionFromFirebase } = require("./fi
 // export 
 
 // controller function to fetch cities data
-async function getCities() {
-    const cities = await getCitiesFromFirebase();
-    return cities;
+async function getCities(req, res) {
+    try {
+        const cities = await getCitiesFromFirebase();
+        res.send(cities);
+    } catch (err) {
+        res.send("Error in fetching cities " +  err);
+    }
 }
 
 // controller function to fetch data from any given collection
-async function getDataOfCollection(collectionName) {
-    const data = await getDataOfCollectionFromFirebase(collectionName);
-    return data;
+async function getDataOfCollection(req, res) {
+    try {
+        const collectionName = req.params.collectionName;
+        const data = await getDataOfCollectionFromFirebase(collectionName);
+        res.send(data);
+    }
+    catch (err) { 
+        res.send("Error in fetching data from collection " + err);
+    }
 }
 
 
